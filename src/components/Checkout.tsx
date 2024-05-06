839a0fd3a9c14f06bed4920cf9c93a50f3d41c75
Sneha Reddy <snehareddy9178@gmail.com>
2024-05-06 23:33:59 +0530

    update ui

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -10,14 +10,14 @@ const Checkout = () => {
   }, []);
 
   return (
-    <div className="flex flex-col gap-2 justify-center items-center my-10">
+    <div className="flex flex-col gap-2 justify-center items-center mt-24">
       <img
-        src="/assets/icons/checkout.png"
+        src="/assets/icons/circle.png"
         alt="checkout"
-        className="h-24 w-24"
+        className="h-24 w-24 shadow-gray-400 shadow-md rounded-full"
       />
-      <div className="text-orange-100">Order placed successfully</div>
-      <p className="px-6 py-[2px] bg-orange-100 hover:bg-orange-400 text-white rounded-2xl text-center text-xs">
+      <div className="text-green-400">Order placed successfully</div>
+      <p className="mt-16 px-6 py-2 bg-orange-100 hover:bg-orange-400 text-white rounded-2xl text-center text-xs">
         <Link to="/products">Shop Now</Link>
       </p>
     </div>
